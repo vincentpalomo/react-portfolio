bf341199014c0b204e4ef14ad89e5d794f7ee423
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-10 21:46:37 -0600

    updated smooth scroll

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -11,7 +11,7 @@ import Lenis from '@studio-freight/lenis';
 
 const Home = () => {
   const lenis = new Lenis({
-    duration: 1.2,
+    duration: 2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     smooth: true,
     smoothTouch: false,
